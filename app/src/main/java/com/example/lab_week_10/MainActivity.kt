a22e641ef9bbefdd078c361c6ee8530ce9448f2c
Araldius <givenmorthen@gmail.com>
2025-11-17 19:37:12 +0700

    Commit No. 1: Implement ViewModel to Handle Rotation

---
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.lab_week_10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import com.example.lab_week_10.viewmodels.TotalViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private val viewModel by lazy {
+        ViewModelProvider(this)[TotalViewModel::class.java]
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Set initial text from ViewModel's total
+        updateText(viewModel.total)
+
+        // Set click listener to increment total in ViewModel
+        findViewById<Button>(R.id.button_increment).setOnClickListener {
+            viewModel.incrementTotal()
+            updateText(viewModel.total)
+        }
+    }
+
+    private fun updateText(total: Int) {
+        findViewById<TextView>(R.id.text_total).text =
+            getString(R.string.text_total, total)
+    }
+}
